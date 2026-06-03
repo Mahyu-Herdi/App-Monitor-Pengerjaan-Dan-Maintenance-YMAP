3564bc86c6b5976d57fef8d69d34df3751430664
TheBugFather <erbeawebird@gmail.com>
2026-06-04 02:12:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // PUSAT PENGATURAN APLIKASI
-const API_URL = "https://script.google.com/macros/s/AKfycbzmBrA8_9QQOZaJ3WPRexSshjQv5niQt3X9B-t92bhHh7jiJtsGH4-oJ0uPmOQ1MBx1/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbypDfTmitFdNGAxb5ye0kjdiaJIVklswCx1d8kCDu5U5Z_ufTYHW3HXCva4YUQ5D5sL/exec";
 
 // Masukkan link gambar logo/Kop Surat (bisa dari Google Drive atau link gambar lain)
 const KOP_SURAT_URL = "kop.png";
