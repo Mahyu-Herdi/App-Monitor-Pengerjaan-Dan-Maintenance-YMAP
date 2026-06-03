ba6c9a5de0c01cd27fe2a0a6a2e83416760ea189
TheBugFather <erbeawebird@gmail.com>
2026-06-03 22:11:58 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// Pusat Konfigurasi API Jembatan Google Sheets
+// Ganti dengan URL Web App Apps Script hasil deploy kamu
+const API_URL = "https://script.google.com/macros/s/AKfycbxeMxWVI7JqpEtSa0OWTe1Lt_jpm9gYBGmIYcR7rqn1XZBQbgCkn-TN9OOvjZDnMI8U/exec";
